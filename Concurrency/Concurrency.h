f92fc117e064db13ba5ea9de8db389681137d85f
Indragie Karunaratne <i@indragie.com>
2019-06-24 12:44:37 -0700

    Convert from SwiftPM to a standard Xcode project

---
+++ Concurrency/Concurrency.h
@@ -0,0 +1,16 @@
+//
+//  Concurrency.h
+//  Concurrency
+//
+//  Created by Indragie Karunaratne on 6/24/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Concurrency.
+FOUNDATION_EXPORT double ConcurrencyVersionNumber;
+
+//! Project version string for Concurrency.
+FOUNDATION_EXPORT const unsigned char ConcurrencyVersionString[];
+
+#import <Concurrency/AtomicBridges.h>
